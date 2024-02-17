0654e0712a7c24181d0a850110ce8dc9a737d080
Marina Lagoikina <marinalagoikina@gmail.com>
2024-02-17 20:31:41 +0300

    fest commit

---
+++ src/main/java/by/home/collection/Main_1.java
@@ -0,0 +1,28 @@
+package by.home.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main_1 {
+    public static void main(String[] args) {
+        Collection<String> c = new ArrayList<>();
+        c.add("marina");
+        c.add("ali");
+        c.add("Vasya");
+        Iterator<String> iter = c.iterator();
+
+        while (iter.hasNext()) {
+            String element = iter.next();
+            System.out.println(element);
+        }
+
+        for (String element : c) {
+            System.out.println(element);
+        }
+
+        Map<String,Integer> f = Map.of();
+
+    }
+}
